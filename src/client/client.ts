e311f035b9b227cb9bd103ed50d5d726157bafc9
Yashwant Raut <yashwantraut41@icloud.com>
2022-12-25 10:56:28 +0530

    small rederer

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,17 +2,23 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
+// scene.background = new THREE.Color(0xddff00f);
 
 const camera = new THREE.PerspectiveCamera(
     75,
-    window.innerWidth / window.innerHeight,
+    // window.innerWidth / window.innerHeight,
+    1,
     0.1,
     1000
 )
 camera.position.z = 2
+const canvas = document.getElementById("c1") as HTMLCanvasElement;
+const renderer = new THREE.WebGLRenderer({canvas:canvas})
+//setSize need to be set in order to get your ThreeJs application running properly in window size 
+// renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setSize(200,200);
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
+//renderer domElement is nothing but html canvas element 
 document.body.appendChild(renderer.domElement)
 
 new OrbitControls(camera,renderer.domElement);
@@ -26,6 +32,7 @@ const material = new THREE.MeshBasicMaterial({
 
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
+console.dir(scene);
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
